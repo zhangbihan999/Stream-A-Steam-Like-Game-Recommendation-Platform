8ef182a8874e3e814a279bb713e938ec9332720c
zhangbihan999 <zhangbihan999@gmail.com>
2024-07-16 13:56:15 +0800

    finish the function in login & register

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,76 @@
+'use client'
+import { supabase } from "@/lib/api";
+import Link from 'next/link';
+import useUserStore from '@/lib/useStore';
+import { useRouter } from 'next/navigation';
+
 export default function Home() {
-    return (
-        <div className="text-red-500">登录界面</div>
-    )
-}
+  const { setUser, user } = useUserStore();  /* 记录用户状态 */
+  const router = useRouter()
+  
+  async function handleSignIn(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();  // 阻止表单的默认提交行为
+    const form = event.target as HTMLFormElement;
+    const name = form.elements.namedItem('name') as HTMLInputElement;
+    const password = form.elements.namedItem('password') as HTMLInputElement;
+
+    // 从数据库中查询是否存在该用户名
+    const { data: userData, error: userError } = await supabase
+        .from('user')
+        .select("*")
+        .eq('name', name.value)
+        .single(); // Assuming 'name' should be unique
+
+    if (userError || !userData) {
+        console.error('Error fetching user:', userError);
+        alert("Account not exists.");
+        return;
+    }
+
+    // 检查密码是否匹配
+    if (userData.password !== password.value) {
+        alert("Password is wrong.");
+        return;
+    }
+
+    setUser({id: userData.id, name: userData.name, password: userData.password})   /* 设置用户信息 */
+    router.push('/dashboard') 
+  }
+
+  return (
+    <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+      {/* <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">登录</h2>
+      </div> */}
+
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form className="space-y-6" action="#" method="POST" onSubmit={handleSignIn}>
+          <div>
+            <div className="flex items-center justify-between">
+              <label htmlFor="name" className="block text-sm font-medium leading-6 text-gray-900">用户名</label>
+              <div className="text-sm">
+                  <Link href="/register" className="font-semibold text-indigo-600 hover:text-indigo-500">点此创建新用户</Link>
+              </div>
+              </div>
+            <div className="mt-2">
+              <input id="name" name="name" type="name" autoComplete="name" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+            </div>
+          </div>
+
+          <div>
+              <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">密码</label>
+            <div className="mt-2">
+              <input id="password" name="password" type="password" autoComplete="current-password" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+            </div>
+          </div>
+
+          <div>
+            <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">登录</button>
+          </div>
+        </form>
+
+      </div>
+    </div>
+    
+  );
+};
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,5 +1,88 @@
+'use client'
+import Link from 'next/link';
+import React, { useState } from 'react';
+import { supabase } from "@/lib/api";
+
 export default function Home() {
+  const [mode, setMode] = useState('User')
+  const [authorityCode, setAuthorityCode] = useState('')
+
+  // 生成八位随机 id
+  function generateEightDigitId() {
+    return Math.floor(Math.random() * 90000000) + 10000000;
+  } 
+
+  // 函数用于向 'user' 表添加新用户
+  async function handleSignUp(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault()  // 禁止刷新界面
+    const form = event.target as HTMLFormElement;
+    const name = form.elements.namedItem('name') as HTMLInputElement;
+    const password = form.elements.namedItem('password') as HTMLInputElement;
+    const check = form.elements.namedItem('check') as HTMLInputElement;
+    const id = generateEightDigitId();
+
+    if (check.value !== password.value) {
+        alert("The two passwords are different")
+        return
+    }
+
+    const { data, error } = await supabase
+        .from('user')
+        .insert([
+            { 
+                id: id,
+                name: name.value,
+                password: password.value,
+            }
+        ]);
+
+    if (error) {
+        console.error('Error inserting data: ', error);
+        alert("Error creating account.");
+    } else {
+        console.log('Data inserted: ', data);
+        alert("Account Created! Let's sign in!");
+        window.location.href = '/login'; // 导航到登录界面
+    }
+}
+
     return (
-        <div className="text-red-500">注册界面</div>
+        <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+        {/* <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">注册</h2>
+        </div> */}
+  
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" action="#" method="POST" onSubmit={handleSignUp}>
+            <div>
+              <div className="flex items-center justify-between">
+                <label htmlFor="name" className="block text-sm font-medium leading-6 text-gray-900">用户名</label>
+              </div>
+              <div className="mt-2">
+                <input id="name" name="name" type="text" autoComplete="name" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+              </div>
+            </div>
+  
+            <div>
+                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">密码</label>
+              <div className="mt-2">
+                <input id="password" name="password" type="password" autoComplete="current-password" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+              </div>
+            </div>
+
+            <div>
+                <label htmlFor="check" className="block text-sm font-medium leading-6 text-gray-900">确认密码</label>
+              <div className="mt-2">
+                <input id="check" name="check" type="password" autoComplete="check" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+              </div>
+            </div>
+  
+            <div>
+              <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">注册</button>
+            </div>
+          </form>
+  
+        </div>
+      </div>
     )
 }
