393c12d718e4d8f90ffa010c476959b6caabf949
zhangbihan999 <zhangbihan999@gmail.com>
2024-07-16 10:59:08 +0800

    init

---
+++ src/app/login/page.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+    return (
+        <div className="text-red-500">登录界面</div>
+    )
+}
---
+++ src/app/register/page.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+    return (
+        <div className="text-red-500">注册界面</div>
+    )
+}
