f9e98a55a9d007472216fa46f7ccd9c1c51ed7e5
zhangbihan999 <zhangbihan999@gmail.com>
2024-07-22 23:25:21 +0800

    补充 RAG.py 重要注释

--- src/app/api/RAG/RAG.py
+++ src/app/api/RAG/RAG.py
@@ -8,7 +8,7 @@ from llama_index.core.query_engine import RetrieverQueryEngine
 from gevent import pywsgi
 import os
 
-# 从环境变量中读取 API 密钥
+# 从环境变量中读取 API 密钥（首先在 python 终端中执行：set MISTRAL_API_KEY=your_api_key）
 api_key = os.environ.get("MISTRAL_API_KEY")
 
 app = Flask(__name__)
